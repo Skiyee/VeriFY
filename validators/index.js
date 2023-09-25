254604a07e4ab8bd28ec6f5d9a612ec69fd24d65
Skiy <319619193@qq.com>
2023-09-25 09:40:00 +0800

    feat(validators): 新增对象校验器

--- validators/index.js
+++ validators/index.js
@@ -1,9 +1,11 @@
 import string from './string'
 import number from './number'
 import array from './array'
+import object from './object'
 
 export default {
   string,
   number,
   array,
+  object,
 }
---
+++ validators/object.js
@@ -0,0 +1,16 @@
+import utils from '../utils'
+import rules from '../rules'
+
+export default function object(rule, value, callback, source, options) {
+  const errors = []
+  const validate = rule.required || Object.prototype.hasOwnProperty.call(source, rule.field)
+  if (validate) {
+    if (utils.isEmpty(value) && !rule.required)
+      return callback([])
+
+    rules.required(rule, value, source, errors, options)
+    if (!utils.isEmpty(value))
+      rules.type(rule, value, source, errors, options)
+  }
+  callback(errors)
+}
