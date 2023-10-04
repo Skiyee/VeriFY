d9c6a02d937f6e4a8805d8fbec2bac1cdfd541b7
sKy <319619193@qq.com>
2023-10-04 17:45:06 +0800

    chore: 更新并配置部分依赖

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,22 @@
+/**
+ * 安装: npm install -D eslint @antfu/eslint-config
+ * 设置: 在.vscode->settings.json设置自动格式化(其他设置package.json已添加)
+ * 使用: hooks上自动触发
+ */
 import antfu from '@antfu/eslint-config'
 
 export default antfu(
+  // 插件的基础配置
+  {
+    typescript: false,
+    ignores: [
+      'dist',
+    ],
+  },
+  // eslint的扁平化配置
   {
     rules: {
+      // 在生产环境下警告不允许使用console
       'no-console': 'off',
     },
   },
