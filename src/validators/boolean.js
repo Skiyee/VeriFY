ba194a01dfd7b88df7b192ba04b7558de8a5d65e
sKy <319619193@qq.com>
2023-10-04 17:40:22 +0800

    feat(validators): 新增布尔值校验器

---
+++ src/validators/boolean.js
@@ -0,0 +1,18 @@
+import rules from '../rules'
+import utils from '../utils'
+
+export default function boolean(rule, value, callback, source, options) {
+  const errors = []
+  const validate = rule.required || Object.prototype.hasOwnProperty.call(source, rule.field)
+
+  if (validate) {
+    if (utils.isEmpty(value) && !rule.required)
+      return callback([])
+
+    rules.required(rule, value, source, errors, options)
+
+    if (!utils.isEmpty(value))
+      rules.type(rule, value, source, errors, options)
+  }
+  callback(errors)
+}
--- src/validators/index.js
+++ src/validators/index.js
@@ -2,10 +2,12 @@ import string from './string'
 import number from './number'
 import array from './array'
 import object from './object'
+import boolean from './boolean'
 
 export default {
   string,
   number,
   array,
   object,
+  boolean,
 }
