03d82c13330da10db1df8f1c4782060e255714e2
sKy <319619193@qq.com>
2023-10-04 17:42:28 +0800

    refactor(validators): 修改number校验器空值判断

--- src/validators/number.js
+++ src/validators/number.js
@@ -4,15 +4,14 @@ import rules from '../rules'
 export default function number(rule, value, callback, source, options) {
   const errors = []
   const validate = rule.required || Object.prototype.hasOwnProperty.call(source, rule.field)
-  if (validate) {
-    if (value === '')
-      value = undefined
 
+  if (validate) {
     if (utils.isEmpty(value) && !rule.required)
       return callback([])
 
     rules.required(rule, value, source, errors, options)
-    if (value !== undefined) {
+
+    if (!utils.isEmpty(value)) {
       rules.type(rule, value, source, errors, options)
       rules.range(rule, value, source, errors, options)
     }
