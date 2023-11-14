1cdc76324ba7bdb7c7be3d18cff5156bfdbfa2e6
sKy <319619193@qq.com>
2023-11-14 14:46:42 +0800

    test: 添加部分新增校验器测试用例

--- example/index.js
+++ example/index.js
@@ -1,4 +1,60 @@
-import Schema from '../dist/verify.es.js'
+import Schema from '../dist/index.mjs'
+
+const patternDesc = {
+  test: { required: true, type: 'string', extend: 'pattern', pattern: '/^[a-z]+$/' },
+}
+
+const patternSource = {
+  test: 123,
+}
+
+const patternValidator = new Schema(patternDesc)
+
+patternValidator.validate(patternSource, (error) => {
+  console.log(error)
+})
+
+const moneyDesc = {
+  test: { required: true, type: 'number', extend: 'money', min: 0, max: 1000 },
+}
+
+const moneySource = {
+  test: 200,
+}
+
+const moneyValidator = new Schema(moneyDesc)
+
+moneyValidator.validate(moneySource, (error) => {
+  console.log(error)
+})
+
+const enumStringDesc = {
+  test: { required: true, type: 'enum', enums: ['one', 'two', 'three'] },
+}
+
+const enumStringSource = {
+  test: '123',
+}
+
+const enumStringValidator = new Schema(enumStringDesc)
+
+enumStringValidator.validate(enumStringSource, (error) => {
+  console.log(error)
+})
+
+const enumArrayDesc = {
+  test: { required: true, type: 'enum', enums: ['one', 'two', 'three'] },
+}
+
+const enumArraySource = {
+  test: ['three', '123'],
+}
+
+const enumArrayValidator = new Schema(enumArrayDesc)
+
+enumArrayValidator.validate(enumArraySource, (error) => {
+  console.log(error)
+})
 
 const booleanDesc = {
   cool: { required: true, type: 'boolean', message: 'super cool' },
